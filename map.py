85d7c6afc4218968f1629f6fdf42834fcc1e06ea
DaviZTS <davitassara@gmail.com>
2024-04-30 11:46:34 -0300

    Implementação volta de sala

--- map.py
+++ map.py
@@ -11,6 +11,11 @@ class Map():
         self.y_correction = 350
         self.off_set_x = 0
         self.off_set_y = 0
+
+        self.pos_x_new_room = 0
+        self.pos_y_new_room = 0
+        self.pos_x_previous_room = 0
+        self.pos_y_previous_room = 0
         pass
 
     #TODO: fazer docstring
@@ -38,13 +43,21 @@ class Map():
                             # Desenha um retângulo vermelho para representar a área de colisão, com uma borda de 1 pixel
                             pygame.draw.rect(screen, (255, 0, 0), rect, 2) 
                         elif obj.name == "New Room":
-                            scaled_x = obj.x * self.scale_factor + self.x_correction
-                            scaled_y = obj.y * self.scale_factor - self.y_correction
+                            self.pos_x_new_room = obj.x * self.scale_factor + self.x_correction
+                            self.pos_y_new_room = obj.y * self.scale_factor - self.y_correction
                             scaled_width = obj.width * self.scale_factor
                             scaled_height = obj.height * self.scale_factor
-                            rect = pygame.Rect(scaled_x + self.off_set_x, scaled_y - self.off_set_y, scaled_width, scaled_height)
-                            # Desenha um retângulo vermelho para representar a área de colisão, com uma borda de 1 pixel
+                            rect = pygame.Rect(self.pos_x_new_room + self.off_set_x, self.pos_y_new_room - self.off_set_y, scaled_width, scaled_height)
+                            # Desenha um retângulo verde para representar a área de colisão, com uma borda de 1 pixel
                             pygame.draw.rect(screen, (0, 255, 0), rect, 2) 
+                        elif obj.name == "Previous Room":
+                            self.pos_x_previous_room = obj.x * self.scale_factor + self.x_correction
+                            self.pos_y_previous_room = obj.y * self.scale_factor - self.y_correction
+                            scaled_width = obj.width * self.scale_factor
+                            scaled_height = obj.height * self.scale_factor
+                            rect = pygame.Rect(self.pos_x_previous_room + self.off_set_x, self.pos_y_previous_room - self.off_set_y, scaled_width, scaled_height)
+                            # Desenha um retângulo verde para representar a área de colisão, com uma borda de 1 pixel
+                            pygame.draw.rect(screen, (0, 0, 255), rect, 2) 
 
     #TODO: fazer docstring
     def check_collision(self, player_rect: pygame.rect) -> bool:
@@ -70,6 +83,22 @@ class Map():
             if isinstance(layer, pytmx.TiledObjectGroup):
                 for obj in layer:
                     if obj.name == "New Room":
+                        rect = pygame.Rect(obj.x * self.scale_factor + self.x_correction + self.off_set_x, obj.y * self.scale_factor - self.y_correction - self.off_set_y, 
+                                           obj.width * self.scale_factor, obj.height * self.scale_factor)
+                        if player_rect.colliderect(rect):
+                            return True
+        return False
+    
+    def check_previous_room(self, player_rect: pygame.rect) -> bool:
+        """Retorna True se o retângulo do player estiver colidindo com um objeto "Previous Room" do map
+        
+        Args:
+            player_rect: retângulo que permite a localização e identificação de colisões do player
+        """
+        for layer in self.tmx_map.visible_layers:
+            if isinstance(layer, pytmx.TiledObjectGroup):
+                for obj in layer:
+                    if obj.name == "Previous Room":
                         rect = pygame.Rect(obj.x * self.scale_factor + self.x_correction + self.off_set_x, obj.y * self.scale_factor - self.y_correction - self.off_set_y, 
                                            obj.width * self.scale_factor, obj.height * self.scale_factor)
                         if player_rect.colliderect(rect):
